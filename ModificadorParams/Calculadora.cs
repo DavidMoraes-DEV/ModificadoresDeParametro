b20f380e5e3454617035a00bceaf6f10895b65d2
David Moraes <investtraderrs@gmail.com>
2020-09-19 16:26:52 -0300

    Modificadores de Parametro

---
+++ ModificadorParams/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModificadorParams
+{
+    class Calculadora
+    {
+        //O params tem como objetivo avisar para a função que queremos parâmetro VARIÁVEIS, ou seja, diferentes tipos de quantidade de parâmetros de entrada
+        public static int Soma (params int[] numbers) //Função de Soma utilizando um Vetor como Parâmetro para a soma.
+        {
+            int soma = 0;
+            for(int i=0; i<numbers.Length; i++) // A Palavra "Length" tem a função de puxar o tamanho do vetor automaticamente
+            {
+                soma += numbers[i];
+            }
+            return soma;
+        }
+    }
+}
---
+++ ModificadorParams/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ModificadorParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculadora.Soma(2, 3 ); //A palavra PARAMS aplicada a função SOMA possibilita que
+                                              //não seja necessario instânciar um vetor na sua chamada podendo
+                                              //inserir diretamente os parâmetros de entrada necessarios para
+                                              //enviar para a função.
+
+            Console.WriteLine(s1);
+        }
+    }
+}
