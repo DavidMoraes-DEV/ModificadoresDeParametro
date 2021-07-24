c5b003a42562c5d5d8af1f9ad06bab651f5dc287
DavidMoraes-DEV <71403807+DavidMoraes-DEV@users.noreply.github.com>
2021-07-24 14:53:03 -0300

    Revisao

--- ModificadorParams/Program.cs
+++ ModificadorParams/Program.cs
@@ -4,14 +4,18 @@ namespace ModificadorParams
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int s1 = Calculadora.Soma(2, 3 ); //A palavra PARAMS aplicada a função SOMA possibilita que
+        static void Main(string[] args) {
+
+
+        int s1 = Calculadora.Soma(new int[] { 20, 30 }); //Utilizando um vetor para enviar os valores desejados
+
+        int s2 = Calculadora.Soma(2, 3 ); //A palavra PARAMS aplicada a função SOMA possibilita que
                                               //não seja necessario instânciar um vetor na sua chamada podendo
                                               //inserir diretamente os parâmetros de entrada necessarios para
                                               //enviar para a função.
 
             Console.WriteLine(s1);
+            Console.WriteLine(s2);
         }
     }
 }
